a7aff6470b4f286dcd32d5f2ac9b097b3aedb427
vduttrat <virottamduttr@gmail.com>
2025-12-05 18:33:59 +0530

    Shifted code to React

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css"
+import CareerlinkCarousel from './components/carousel';
+import CareerlinkNavbar from './components/navbar';
+import CareerlinkFormTable from './components/formtableinteraction';
+import Hero from './components/hero';
+import Sidebar from './components/sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CareerlinkNavbar />
+    <Hero />
+    <Sidebar />
+    <div style={{width:"75vw", position:"absolute", left:"20vw", top:"102vh"}}>
+        <CareerlinkCarousel />
+        <CareerlinkFormTable />
+    </div>
   </React.StrictMode>
 );
 
